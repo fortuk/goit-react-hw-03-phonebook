edb2beebc60b9808ff88a9e2b3cf971b2009cd36
Алексей <fortuk22@rambler.ru>
2022-05-28 23:51:30 +0300

    пропись локал сторедж

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './component/App/App';
+import App from '../src/components/App/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    App / >
+    <
+    /React.StrictMode>,
+    document.getElementById('root'),
 );
